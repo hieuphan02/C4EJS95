fbf9d167dee37756dc218979e7f87dccffbf1988
hieuphan02 <hp190702@gmail.com>
2020-08-29 19:59:44 +0700

    commit lan 1

---
+++ index/C4EJS95-Phan Trọng Hiếu-day1/index.js
@@ -0,0 +1,28 @@
+let message ="Coding is great";
+console.log (message) ;
+let studentCount ="0";
+console.log(studentCount);
+message = "Coding might not be easy, but still great"
+console.log (message)
+studentCount = 16
+console.log(studentCount)
+message = "coding might not be easy, but still great"
+console.log(message)
+studentCount += 1
+console.log(studentCount)
+alert("you look good")
+let userName = prompt("Hi there, your name please?")
+alert("Hi " + userName + "!")
+let firstName = prompt("Enter your first name")
+let lastName = prompt("Enter your last name")
+alert("Hi " + lastName +" "+ firstName)
+let squareSideLength = prompt("Please enter the side length of the square")
+let squareArea = squareSideLength * squareSideLength
+alert("The square area is" + squareArea )
+let radiusCircle = prompt ("Enter the radius of the circle")
+let circleArea = radiusCircle * 3.14 * 3.14
+alert("The circle area is " + circleArea )
+let tempC = prompt("Enter the temperature in Celsius")
+let tempF = (tempC *9/5 )+32
+alert(tempC + "C"+ "=" + tempF +"F")
+// end
